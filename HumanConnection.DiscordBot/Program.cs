344ecbd49ced86a0deabe59ff5ac735d777a4264
Lala Sabathil <pkmoras.facebook@web.de>
2018-09-21 00:28:43 +0200

    Preparing for master release

--- HumanConnection.DiscordBot/Program.cs
+++ HumanConnection.DiscordBot/Program.cs
@@ -10,23 +10,26 @@ namespace HumanConnection.DiscordBot
     public class Program
     {
         private static Mutex INSTANCE_MUTEX = new Mutex(true, "HC Control");
+
         public static HCBotGUI BOT_UI = new HCBotGUI();
         private static HCBotConsole BOT = new HCBotConsole();
+
         static void Main()
         {
-            // Check if an instance is already running. Remove this block if you want to run multiple instances.
             if (!INSTANCE_MUTEX.WaitOne(TimeSpan.Zero, false))
             {
                 MessageBox.Show("The applicaton is already running.");
                 return;
             }
-            // Start the UI.
+
             try {
                 Application.Run(BOT_UI as Form);
             }
-            catch { Console.WriteLine("Failed to run."); }
+            catch {
+                Console.WriteLine("Failed to run.");
+            }
         }
-        // Connect to the bot, or cancel before the connection happens.
+
         public static void Run() => Task.Run(() => BOT.RunAsync(BOT_UI.GetToken));
         public static void Cancel() => Task.Run(() => BOT.CancelAsync());
         public static void Stop() => Task.Run((Func<Task>)(() => BOT.StopAsync()));

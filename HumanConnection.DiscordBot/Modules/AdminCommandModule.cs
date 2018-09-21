bd11128237bda7bc004a2c5aeb05335cb82e7a15
Lala Sabathil <pkmoras.facebook@web.de>
2018-09-21 14:49:28 +0200

    Not funcational

--- HumanConnection.DiscordBot/Modules/AdminCommandModule.cs
+++ HumanConnection.DiscordBot/Modules/AdminCommandModule.cs
@@ -28,37 +28,72 @@ namespace HumanConnection.DiscordBot.Modules
             SocketGuildUser author = (SocketGuildUser)message.Author;
             var on = String.Empty;
             var command = GetCommand(message);
-            if (author.GuildPermissions.Administrator)
-            {
-                if (command[0] == "!kick")
-                {
-                    var reason = command[1];
-                    var userToKick = message.MentionedUsers.ElementAt(0) as SocketGuildUser;
-                    await Kick(message, userToKick, reason, logChannel);
-                }
-            } else
+            /*if (author.GuildPermissions.Administrator)
+            {*/
+                await ExecuteCommand(command, message, author);
+            /*}
+            else
             {
                 if (message.MentionedUsers.Count != 0)
                 {
                     on = $" on {message.MentionedUsers.ElementAt(0).Username}";
                 }
                 await logChannel.SendMessageAsync($"**Alert**\n{author.Username} tried to execute admin command `{command[0]}`{on}\nFull requested command context: `{command[1]}`");
-            }
+            }*/
 
             Program.DelMsg(message.Channel, message.Id);
         }
 
-        private async Task Kick(SocketMessage message, SocketGuildUser user, string reason, SocketTextChannel channel)
+        private async Task ExecuteCommand(string[] command, SocketMessage message, SocketGuildUser author)
+        {
+            Program.Log(new LogMessage(LogSeverity.Info, "Admin Module", $"{command[0]} | {command[1]}"));
+            if (command[0] == "!kick")
+            {
+                Program.Log(new LogMessage(LogSeverity.Info, "Admin Module", $"{command[0]} | {command[1]}"));
+                var reason = command[1];
+                var guildUser = message.MentionedUsers.ElementAt(0) as SocketGuildUser;
+                await Kick(guildUser, reason, logChannel);
+            }
+            else if (command[0] == "!ban")
+            {
+                Program.Log(new LogMessage(LogSeverity.Info, "Admin Module", $"{command[0]} | {command[1]}"));
+                var vari = TrimIt(command[1], ' ', 2);
+                var reason = vari[1];
+                var timespan = Convert.ToInt16(vari[0]);
+                var guildUser = message.MentionedUsers.ElementAt(0) as SocketGuildUser;
+                await Ban(guildUser, timespan, reason, logChannel);
+            }
+            else
+            {
+                Program.Log(new LogMessage(LogSeverity.Info, "Admin Module", $"{command[0]} | {command[1]}"));
+                Console.WriteLine($"Unkown command '{command[0]}'");
+            }
+        }
+
+        private async Task Kick(SocketGuildUser user, string reason, SocketTextChannel channel)
         {
             await channel.SendMessageAsync($"**Kick**\n{user.Mention} ({user.Username}) with reason `{reason}`");
-            await user.KickAsync();
+            await user.KickAsync(reason);
+        }
+
+        private async Task Ban(SocketGuildUser user, int length, string reason, SocketTextChannel channel)
+        {
+            await channel.SendMessageAsync($"**Ban**\n{user.Mention} ({user.Username}) with reason `{reason}` and deletes messages from the last {length} days");
+            await user.Guild.AddBanAsync(user as IUser, length, reason);
+        }
+
+        private string[] TrimIt(string text, char delemite, int length)
+        {
+            string[] stringArr = text.Split(new char[] { delemite }, length);
+
+            return stringArr;
         }
 
         private string[] GetCommand(SocketMessage msg)
         {
             var fullCommand = msg.Content;
 
-            var cmd = fullCommand.Split(new char[] { ' ' }, 2);
+            string[] cmd = fullCommand.Split(new char[] { ' ' }, 2);
 
             return cmd;
         }

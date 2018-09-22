977bd50d97de7c29cb9962d3f56764542f6add20
Lala Sabathil <pkmoras.facebook@web.de>
2018-09-22 21:01:53 +0200

    Role Module base

---
+++ HC-DBot/Modules/RoleModule.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static HC_DBot.GuildStatics;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace HC_DBot.Modules
+{
+    class RoleModule : BaseCommandModule
+    {
+
+    }
+}

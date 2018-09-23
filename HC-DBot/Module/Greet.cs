e10253a68a99ea79cd8f014c9f6c467229eabee2
Lala Sabathil <pkmoras.facebook@web.de>
2018-09-23 18:38:16 +0200

    Added forogtten files :')

---
+++ HC-DBot/Module/Greet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HC_DBot.Module
+{
+    class Greet
+    {
+        public Greet()
+        {
+            // Empty
+        }
+    }
+}

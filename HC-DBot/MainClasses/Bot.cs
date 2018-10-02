7c53c1ba8a5c81596c0c6596c35d7c1c0e3087b7
Lala Sabathil <pkmoras.facebook@web.de>
2018-10-02 15:14:35 +0200

    changes on memberjoin & added memberleave

--- HC-DBot/MainClasses/Bot.cs
+++ HC-DBot/MainClasses/Bot.cs
@@ -38,6 +38,7 @@ namespace HC_DBot.MainClasses
             Client = new DiscordClient(cfg);
             Client.GuildDownloadCompleted += BotGuildsDownloaded;
             Client.GuildMemberAdded += MemberAdd;
+            Client.GuildMemberRemoved += MemberLeave;
             Client.GuildCreated += BotGuildAdded;
             //Client.MessageReactionAdded += ReactAdd;
             //Client.MessageReactionRemoved += ReactRemove;
@@ -558,19 +559,39 @@ namespace HC_DBot.MainClasses
             await connection.CloseAsync();
         }
 
+        public async Task MemberLeave(GuildMemberRemoveEventArgs e)
+        {
+            await connection.OpenAsync();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand
+                {
+                    Connection = connection,
+                    CommandText = $"DELETE FROM `guilds.users` WHERE userName=userName"
+                };
+                cmd.Parameters.Add("userName", MySqlDbType.VarChar).Value = e.Member.Username;
+                await cmd.ExecuteNonQueryAsync();
+                GuildsList[e.Guild.Id].GuildMembers.Remove(e.Member.Id);
+                await LogAction(e.Guild, "MemberLeave", "Fired on member leave", $"User {e.Member.Username} leaved", DiscordColor.IndianRed);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                Console.WriteLine(ex.StackTrace);
+            }
+            await connection.CloseAsync();
+        }
+
         public async Task GreetUser(GuildMemberAddEventArgs e)
         {
-            if (GuildsList[e.Guild.Id].ModuleConfig.GreetModule)
-            {
-                var msg = await e.Member.SendMessageAsync($"Welcome {e.Member.Mention}\n" +
-                $"You succesfully landed on {e.Guild.Name} \n\n" +
-                $"Please take a look into {e.Guild.GetChannel(GuildsList[e.Guild.Id].ChannelConfig.InfoChannelID).Mention} for informations regarding this server.\n" +
-                $"To accept the rules ({e.Guild.GetChannel(GuildsList[e.Guild.Id].ChannelConfig.RuleChannelID).Mention}), please write `$accept-rules` in {e.Guild.GetChannel(GuildsList[e.Guild.Id].ChannelConfig.CmdChannelID).Mention}.\n" +
-                $"You will automatically get assigned to the role *{e.Guild.GetRole(GuildsList[e.Guild.Id].ChannelConfig.RoleID).Name}*.\n\n" +
-                $"{GuildsList[e.Guild.Id].ChannelConfig.CustomInfo}");
+            await e.Member.SendMessageAsync($"Welcome {e.Member.Mention}\n" +
+            $"You succesfully landed on {e.Guild.Name} \n\n" +
+            $"Please take a look into {e.Guild.GetChannel(GuildsList[e.Guild.Id].ChannelConfig.InfoChannelID).Mention} for informations regarding this server.\n" +
+            $"To accept the rules ({e.Guild.GetChannel(GuildsList[e.Guild.Id].ChannelConfig.RuleChannelID).Mention}), please write `$accept-rules` in {e.Guild.GetChannel(GuildsList[e.Guild.Id].ChannelConfig.CmdChannelID).Mention}.\n" +
+            $"You will automatically get assigned to the role *{e.Guild.GetRole(GuildsList[e.Guild.Id].ChannelConfig.RoleID).Name}*.\n\n" +
+            $"{GuildsList[e.Guild.Id].ChannelConfig.CustomInfo}");
                 
-                await LogAction(e.Guild, msg, "GreetUser", "Greet's the given user on join", $"User {e.Member.Mention} was greeted", DiscordColor.Green);
-            }
+            await LogAction(e.Guild, "GreetUser", "Greet's the given user on join", $"User {e.Member.Mention} was greeted", DiscordColor.Green);
         }
 
         /*public async Task ReactAdd(MessageReactionAddEventArgs e)
@@ -616,6 +637,32 @@ namespace HC_DBot.MainClasses
             // Build footer
             builder.WithFooter("Copyright 2018 Lala Sabathil");
             builder.WithTimestamp(msg.CreationTimestamp);
+
+            await channel.SendFileAsync(fileName: "logthumbnail.png", fileData: dataStream, content: null, tts: false, embed: builder.Build());
+        }
+
+        public async Task LogAction(DiscordGuild guild, string functionName, string description, string message, DiscordColor color)
+        {
+
+            DiscordChannel channel = guild.GetChannel(GuildsList[guild.Id].ChannelConfig.LogChannelID);
+
+            WebRequest request = WebRequest.Create($"https://png2.kisspng.com/sh/ae7a514d72b233a0ccf5aff823ba701f/L0KzQYm3VMAzN5J0fZH0aYP2gLBuTfcue6ZujNc2Z3Byd73sTgN6e6VqhZ9qZH3sfrr6lQJifJD3ReV4ZoT6ccPsTfRmeJ10RdNtbXnxecT7kvF1d6MyTdNsMnHlRIjoWcZmPmozTKo7N0K7QYK4VcIzP2E8Sqk6Nkm3PsH1h5==/kisspng-g-suite-google-system-administrator-software-deplo-administrator-5ac2ab47a96e69.482728111522707271694.png");
+            WebResponse response = await request.GetResponseAsync();
+            Stream dataStream = response.GetResponseStream();
+
+            // Init builder
+            DiscordEmbedBuilder builder = new DiscordEmbedBuilder();
+            builder.WithColor(color);
+            // Build Header
+            builder.WithTitle("Changelog");
+            builder.WithDescription("Logged user/bot action");
+            builder.WithThumbnailUrl("attachment://logthumbnail.png");
+            // Build content
+            builder.AddField(name: "Function", value: $"{functionName}");
+            builder.AddField(name: "Description", value: $"{description}");
+            builder.AddField(name: "Message", value: $"{message}");
+            // Build footer
+            builder.WithFooter("Copyright 2018 Lala Sabathil");
 
             await channel.SendFileAsync(fileName: "logthumbnail.png", fileData: dataStream, content: null, tts: false, embed: builder.Build());
         }
